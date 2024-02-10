926ab19d595fe24c6f9b5950bc031d89727c7b81
vidisha <vidishanarnaware20@gmail.com>
2024-02-10 16:03:41 +0530

    dddd

--- src/main/java/com/firstservice/controller/FisrtController.java
+++ src/main/java/com/firstservice/controller/FisrtController.java
@@ -1,6 +1,7 @@
 package com.firstservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,6 +20,9 @@ public class FisrtController {
 	@Autowired
 	private WebClient webClient;
 
+	@Value("${base.url.second}")
+	private String baseUrlSecond;
+	
 	@GetMapping
 	public ResponseEntity<String> getMessage() {
 		return new ResponseEntity<String>("hello, From First Service", HttpStatus.OK);
@@ -27,7 +31,7 @@ public class FisrtController {
 	@GetMapping("/call")
 	public ResponseEntity<String> getCallMessage() {
 
-		Mono<String> mono = webClient.get().uri("http://second:8084/second/new").retrieve().bodyToMono(String.class);
+		Mono<String> mono = webClient.get().uri(baseUrlSecond+"/second/new").retrieve().bodyToMono(String.class);
 
 		return new ResponseEntity<String>("First Service Kya bat bava !!!!!!!   " + mono.block(), HttpStatus.OK);
 	}
